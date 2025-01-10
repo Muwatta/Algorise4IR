13d41e0be9ba4fb5ab44ce03767289fb07fe6bf4
Muwatta <abdullahmusliudeen@gmail.com>
2025-01-10 13:34:58 +0100

    Updates

--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export interface algorisetechexplorers {
-  id: number;
-  title: string;
-  description: string;
-  image: string;
-  time: string;
-}
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/Algorise4IR/', // Correct base path for GitHub Pages
-  plugins: [react()],
-});
